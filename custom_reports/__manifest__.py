63bd91fdb14aab3ed388add90dcc283e32acbeb5
nxk <neo.kirbis@gmail.com>
2024-12-03 01:42:37 +0100

    Created custom_reports scaffold and starting point

---
+++ custom_reports/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "custom_reports",
+
+    'summary': "Short (1 phrase/line) summary of the module's purpose",
+
+    'description': """
+Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "https://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'report/sale_report_inherit.xml' 
+    ],
+    'installable': True,
+    'application': False 
+
+}
+
